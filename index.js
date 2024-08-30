42bc980f3cbe0d2c0ba274cdf721120abc6acff5
manoj barange <manojbrnj111@gmail.com>
2024-08-30 18:53:42 +0530

    stun ice socketio dewar todo

--- index.js
+++ index.js
@@ -16,6 +16,10 @@ app.use(
   }),
 );
 
+io.on('connection', (socket) => {
+  console.log('A user connected');
+});
+
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
